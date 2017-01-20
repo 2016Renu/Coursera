95e2ba093e9ec7d4761f1de081f4c6100eca8f86
2016Renu <renupalanichamy@gmail.com>
2017-01-20 09:48:56 -0600

    Add files via upload

---
+++ module4-solution/src/items.component.js
@@ -0,0 +1,11 @@
+(function () {
+    'use strict';
+
+    angular.module('Data')
+        .component('itemsList', {
+            templateUrl: 'src/templates/itemsList.template.html',
+            bindings: {
+                menuItems: '<'
+            }
+        });
+})();
